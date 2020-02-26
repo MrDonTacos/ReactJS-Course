4d9f0c14ebe5c478bece5a6a05bf3ba79c8b2c23
Saul Canedo Castillo <saul.canedocast@hotmail.com>
2020-02-26 08:28:48 -0800

    Proyecto React

---
+++ public/scripts/app.js
@@ -0,0 +1,107 @@
+"use strict";
+
+//Create app object title/subtitle
+//use title/subtyitle in the template
+//render the template
+
+var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Sa\xFAl Ca\xF1edo Castillo"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item Two"
+        )
+    )
+);
+
+var user = {
+    name: "Saúl Cañedo Castillo",
+    age: 21,
+    location: 'Mexicali'
+};
+
+var app = {
+    Title: "Eternal Sunshine",
+    SubTitle: "Jim Carrey"
+};
+
+var userName = 'Saúl Cañedo Castillo';
+var userAge = 21;
+var userLocation = 'Mexico';
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        getLocation(user.location)
+    )
+);
+
+var getLocation = function getLocation(location) {
+    if (location) {
+        return location;
+    } else {
+        return 'unknown';
+    }
+};
+
+var templateThree = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.Title
+    ),
+    React.createElement(
+        "h2",
+        null,
+        app.SubTitle
+    )
+);
+
+// create my own template
+// create a new Template var jsx expression
+// root element
+// h1 => Saul Canedo Castillo
+// p => add Age: 21
+// p => Location: Mexicali
+// Render template Two insted of template
+
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
---
+++ src/app.js
@@ -0,0 +1,67 @@
+//Create app object title/subtitle
+//use title/subtyitle in the template
+//render the template
+
+var template = (
+    <div>
+    <h1>Saúl Cañedo Castillo</h1> 
+    <p>This is some info</p>
+    <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+    </ol>
+    </div>
+    ); 
+
+    var user = {
+        name: "Saúl Cañedo Castillo",
+        age: 21,
+        location: 'Mexicali'
+    }
+
+    var app = {
+        Title: "Eternal Sunshine",
+        SubTitle: "Jim Carrey"
+    }
+
+var userName = 'Saúl Cañedo Castillo'
+var userAge = 21;
+var userLocation = 'Mexico'
+
+const templateTwo = (
+    <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {getLocation(user.location)}</p>
+    </div>
+);
+
+let getLocation = (location) => {
+    if(location){
+        return location
+    }else{
+        return 'unknown'
+    }
+
+}
+
+const templateThree = (
+    <div>
+        <h1>{app.Title}</h1>
+        <h2>{app.SubTitle}</h2>
+    
+    </div>
+);
+
+    // create my own template
+    // create a new Template var jsx expression
+    // root element
+    // h1 => Saul Canedo Castillo
+    // p => add Age: 21
+    // p => Location: Mexicali
+    // Render template Two insted of template
+
+
+    var appRoot = document.getElementById('app');
+
+    ReactDOM.render(templateTwo, appRoot);
