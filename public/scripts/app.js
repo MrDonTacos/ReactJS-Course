773a226620f95dc9275045a839bb3db6fcc6eb72
Saul Canedo Castillo <saul.canedocast@hotmail.com>
2020-02-26 13:28:10 -0800

    Working with virtual DOM

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,18 +4,22 @@
 //use title/subtyitle in the template
 //render the template
 var template = React.createElement("div", null, React.createElement("h1", null, "Sa\xFAl Ca\xF1edo Castillo"), React.createElement("p", null, "This is some info"), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two")));
-var user = {
+var user = [{
   name: "Saúl Cañedo Castillo",
   age: 21,
   location: 'Mexicali'
-};
+}, {
+  name: "Osvaldo Cañedo Castillo",
+  age: 24,
+  location: 'Mexicali'
+}];
 var app = {
   Title: "Eternal Sunshine",
   SubTitle: "Jim Carrey"
-};
-var userName = 'Saúl Cañedo Castillo';
-var userAge = 21;
-var userLocation = 'Mexico';
+}; // var userName = 'Saúl Cañedo Castillo'
+// var userAge = 21;
+// var userLocation = 'Mexico'
+
 var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name), React.createElement("p", null, "Age: ", user.age), React.createElement("p", null, "Location: ", user.location));
 
 var getLocation = function getLocation(location) {
@@ -26,7 +30,40 @@ var getLocation = function getLocation(location) {
   }
 };
 
-var templateThree = React.createElement("div", null, React.createElement("h1", null, app.Title), React.createElement("h2", null, app.SubTitle)); // create my own template
+var templateThree = user.map(function (element) {
+  React.createElement("div", null, React.createElement("h1", null, element.name), React.createElement("h2", null, element.age), console.log(element.age));
+});
+var count = 0;
+
+var addOne = function addOne() {
+  count += 1;
+  renderThings();
+};
+
+var minus = function minus() {
+  count -= 1;
+  renderThings();
+};
+
+var reset = function reset() {
+  count = 0;
+  renderThings();
+};
+
+var appRoot = document.getElementById('app');
+
+var renderThings = function renderThings() {
+  var templateFour = React.createElement("div", null, React.createElement("h1", null, "Count: ", count), React.createElement("button", {
+    onClick: addOne
+  }, "+1"), React.createElement("button", {
+    onClick: minus
+  }, "-1"), React.createElement("button", {
+    onClick: reset
+  }, "0"));
+  ReactDOM.render(templateFour, appRoot);
+};
+
+renderThings(); // create my own template
 // create a new Template var jsx expression
 // root element
 // h1 => Saul Canedo Castillo
@@ -34,5 +71,4 @@ var templateThree = React.createElement("div", null, React.createElement("h1", n
 // p => Location: Mexicali
 // Render template Two insted of template
 
-var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+console.log(templateThree);
--- src/app.js
+++ src/app.js
@@ -13,20 +13,29 @@ var template = (
     </div>
     ); 
 
-    var user = {
-        name: "Saúl Cañedo Castillo",
-        age: 21,
-        location: 'Mexicali'
-    }
+    const user = [
+{
+
+    name: "Saúl Cañedo Castillo",
+    age: 21,
+    location: 'Mexicali'
+},
+{
+    name: "Osvaldo Cañedo Castillo",
+    age: 24,
+    location: 'Mexicali'
+}
+    ]
+    
 
     var app = {
         Title: "Eternal Sunshine",
         SubTitle: "Jim Carrey"
     }
 
-var userName = 'Saúl Cañedo Castillo'
-var userAge = 21;
-var userLocation = 'Mexico'
+// var userName = 'Saúl Cañedo Castillo'
+// var userAge = 21;
+// var userLocation = 'Mexico'
 
 const templateTwo = (
     <div>
@@ -46,13 +55,58 @@ let getLocation = (location) => {
 }
 
 const templateThree = (
-    <div>
-        <h1>{app.Title}</h1>
-        <h2>{app.SubTitle}</h2>
-    
-    </div>
+
+    user.map((element) => {  
+      <div>
+      <h1>{element.name}</h1>
+      <h2>{element.age}</h2>
+      {console.log(element.age)}
+      </div>
+    }) 
+   
 );
 
+
+
+let count = 0;
+
+
+
+let addOne = () => {
+    count+=1;
+    renderThings();
+}
+
+let minus = () => {
+    count-=1;
+    renderThings();
+}
+
+let reset = () => {
+    count=0;
+    renderThings();
+}
+
+var appRoot = document.getElementById('app');
+
+const renderThings = () => {
+
+const templateFour = (
+
+<div>
+<h1>Count: {count}</h1>
+<button onClick={addOne}>+1</button>
+<button onClick={minus}>-1</button>
+<button onClick={reset}>0</button>
+</div>
+
+);
+
+
+ReactDOM.render(templateFour, appRoot);
+};
+
+renderThings();
     // create my own template
     // create a new Template var jsx expression
     // root element
@@ -62,6 +116,7 @@ const templateThree = (
     // Render template Two insted of template
 
 
-    var appRoot = document.getElementById('app');
+  
+    console.log(templateThree);
 
-    ReactDOM.render(templateTwo, appRoot);
+  
